7eacabf1342eeecc2327bcd5b4ec74facbe27a8c
Clay Smith <uhclay@gmail.com>
2015-06-07 15:16:57 -0500

    Add express app with basic search

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var elasticsearch = require('elasticsearch');
+
+var client = new elasticsearch.Client({
+      host: 'localhost:9200',
+      log: 'trace'
+});
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.post('/search', function(req, res, next) {
+  client.search({
+    q: req.body.query
+  }).then(function (body) {
+    console.log(JSON.stringify(body));
+    res.render('search', { title: 'Search Results', searchResults: body.hits.hits.map(function(hit) {return hit._source; })});
+  }, function (error) {
+    console.trace(error.message);
+  });
+  
+});
+
+module.exports = router;
